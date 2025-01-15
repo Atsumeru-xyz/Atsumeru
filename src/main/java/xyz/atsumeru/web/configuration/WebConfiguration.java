3aeaf48464a3f9a122d121b3b487e5a8f2bbfde9
AtsumeruDev <atsumeru.dev@gmail.com>
2025-01-15 13:40:43 +0200

    Explicitly allow trailing slash match

---
+++ src/main/java/xyz/atsumeru/web/configuration/WebConfiguration.java
@@ -0,0 +1,14 @@
+package xyz.atsumeru.web.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseTrailingSlashMatch(true);
+    }
+}
