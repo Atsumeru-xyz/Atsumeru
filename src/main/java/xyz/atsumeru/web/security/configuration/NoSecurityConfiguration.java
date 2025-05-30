3902b7409ede546a70f53b37f54a576fdb00f724
AtsumeruDev <atsumeru.dev@gmail.com>
2025-05-30 17:23:12 +0300

    Implement zero-security configuration for dev profile

--- src/main/java/xyz/atsumeru/web/security/configuration/NoSecurityConfiguration.java
+++ src/main/java/xyz/atsumeru/web/security/configuration/NoSecurityConfiguration.java
@@ -3,7 +3,6 @@ package xyz.atsumeru.web.security.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,28 +11,24 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Profile("!dev")
+@Profile("dev")
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-public class WebSecurityConfiguration {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(
-                        auth -> auth.requestMatchers("/admin/**")
-                                .hasRole("ADMIN")
-                                .anyRequest()
-                                .authenticated()
-                )
-                .httpBasic(Customizer.withDefaults())
-                .cors(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable)
-                .build();
-    }
+public class NoSecurityConfiguration {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(
+                        auth -> auth.anyRequest().permitAll()
+                )
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
     }
 }
